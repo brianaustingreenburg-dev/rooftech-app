db5a150ebbcf9ab1a28bc117fd9c3327437044af
LA Studios <lastudios.ops@gmail.com>
2026-09-13 11:27:05 +0000

    Restore working app - satellite + all original features + new pricing + tax + payroll

--- sw.js
+++ sw.js
@@ -1,9 +1,6 @@
-const CACHE_NAME = 'rooftech-v3';
+const CACHE_NAME = 'rooftech-v4';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    .then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
